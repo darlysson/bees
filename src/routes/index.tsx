7ce8de3f57284ff4013ee2b6fae3dd566aa318dd
Darlysson Alves <alvesd.dev@gmail.com>
2025-08-05 15:30:57 -0300

    feat: style home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,26 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className='text-yellow-300 bg-amber-50'>Your favorite breweries</div>
+    <div className="bg-[#F2EC54] h-screen flex flex-col items-center justify-center relative">
+      <p className='font-normal text-sm mb-4'>Please, enter your full name below</p>
+      <p className='font-normal text-sm mb-4'>Only alphabetical characters are accepted</p>
+
+      <label htmlFor="full-name" className='block mb-4 w-[300px]'>
+        <input placeholder='Your full name' name='full-name' className='border  border-gray-300 bg-white px-3 py-2.5 w-full rounded-sm' />
+      </label>
+
+      <label htmlFor="age" className='flex gap-2 mb-6 font-normal text-sm'>
+        <input type="checkbox" name="age" />
+        Are you older than 18 years old?
+      </label>
+
+      <div className="flex items-center justify-center">
+        <button className='bg-slate-600 px-4 py-2 text-white rounded-sm cursor-pointer text-sm'>Enter</button>
+      </div>
+
+      <div className='absolute bottom-4 left-7'>
+        <img src="/bee.svg" alt="" />
+      </div>
+    </div>
   )
 }
